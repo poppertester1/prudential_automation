2fcae29af80a4e24a890c58e5e7b84eb5d76e58f
Stephen veronica <stephenveronica@Stephens-MacBook-Air.local>
2018-10-11 09:25:19 +0530

    adding project

---
+++ all_imports.py
@@ -0,0 +1,5 @@
+# _*_ coding: utf-8 _*_
+from selenium import webdriver
+import csv
+import pytest
+import unittest
---
+++ tests/test_prudential.py
@@ -0,0 +1,28 @@
+# _*_ coding: utf-8 _*_
+from all_imports import *
+url = 'https://openweathermap.org/'
+
+driver = webdriver.Chrome()
+driver.get(url)
+
+class TestPublishDrafts(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_one(self):
+        pass
+
+    def test_two(self):
+        pass
+
+    def test_three(self):
+        pass
+
+    def test_four(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
